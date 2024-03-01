7a16b4011608d89d554ce0241341204489129e25
Luo Siyuan <75154403+Teacherhand123@users.noreply.github.com>
2024-03-01 10:24:06 +0800

    Add files via upload

--- backend/connectMysql/urls.py
+++ backend/connectMysql/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("checkusers", views.checkUser, name="checkAllUsers"),
+    path("verifylogin", views.verifyLogin, name="checkAllUsers"),
     path("register", views.createNewUser, name="registerNewUsers"),
+    path("verifylogintime", views.verifyLoginTime, name="verifyLoginTime")
 ]
--- backend/connectMysql/views.py
+++ backend/connectMysql/views.py
@@ -1,25 +1,75 @@
+import json
+import hashlib
+import os
+
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.core import serializers
-import json
 
 from .models import userInformation
 
+current_directory = os.path.dirname(os.path.abspath(__file__))
 
 # Create your views here.
 
 
-def checkUser(request):
+def verifyLoginTime(request):
+    if request.method == "POST":
+        postJson = json.loads(request.body.decode())
+        f = open(current_directory + "/userJsonData/userToken.json")
+        tempStr = f.read()
+        f.close()
+        userTokens = json.loads(tempStr)
+
+        userName = ''
+        # check if username has existed
+        for userData in userTokens:
+            if userData["token"] == postJson["userLoginToken"]:
+                userName = userData["username"]
+
+        if userName == '':
+            return JsonResponse({"error": "true"}, safe=False)
+
+        return JsonResponse({"error": "false", "username": userName}, safe=False)
+
+def verifyLogin(request):
     if request.method == "POST":
         postJson = json.loads(request.body.decode())
         try:
             data = json.loads(serializers.serialize("json", userInformation.objects.filter(username=postJson["username"])))
             # check if the same password
             if data[0]["fields"]["password"] == postJson["password"]:
-                return HttpResponse("welcome")
+
+                # dm5 encode to token
+                dm5Encode = hashlib.md5(postJson["password"].encode()).hexdigest()
+
+                f = open(current_directory + "/userJsonData/userToken.json")
+                tempStr = f.read()
+                f.close()
+                userTokens = json.loads(tempStr)
+
+                isNull = True
+                # check if username has existed
+                for userData in userTokens:
+                    if userData["username"] == postJson["username"]:
+                        print(userData)
+                        isNull = False
+                        userData["token"] = dm5Encode
+
+                if isNull:
+                    userTokens.append({"username": postJson["username"],
+                                      "token": dm5Encode})
+
+                # write into .json
+                newTempStr = json.dumps(userTokens)
+                f = open(current_directory + "/userJsonData/userToken.json", 'w')
+                f.write(newTempStr)
+                f.close()
+
+                return JsonResponse({"error": "false", "userlogintoken": dm5Encode, "description": "None"}, safe=False)
         except:
-            return HttpResponse(status=404)
-    return HttpResponse("fail")
+            return JsonResponse({"error": "true", "description": "this user does not exist"}, safe=False)
+    return JsonResponse({"error": "true", "description": "username or password error"}, safe=False)
 
 
 def createNewUser(request):
